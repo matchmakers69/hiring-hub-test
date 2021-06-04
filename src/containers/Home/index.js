e7ef4730c90da662e6182e161a2336e9df887365
Przemek Lewtak <przemek.lewtak@orcha.co.uk>
2021-06-04 14:00:40 +0100

    Created start container Home.js

---
+++ src/containers/Home/index.js
@@ -0,0 +1,15 @@
+import React from "react";
+import PropTypes from "prop-types";
+import JobsMatcher from "../../components/JobsMatcher";
+
+const Home = () => {
+  return (
+    <>
+      <JobsMatcher />
+    </>
+  );
+};
+
+Home.propTypes = {};
+
+export default Home;
